1546e8a23d9f28eaf09fa1f6339dbfcd3421bced
pdrmenezes <pdrmenezes1@gmail.com>
2023-11-06 20:06:32 -0300

    setup authprovider to get session on client side

--- src/app/private-client/page.tsx
+++ src/app/private-client/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 export default function ClientPage() {
-  return <h1>Private Server Session</h1>;
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/api/auth/signin?callbackUrl=/private-client");
+    },
+  });
+  return (
+    <>
+      <h1>Private Server Session</h1>
+      <p>{session.user?.email}</p>
+      <p>{session.user?.role}</p>
+    </>
+  );
 }
