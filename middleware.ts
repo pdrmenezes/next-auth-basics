9ecc33b78aa7004568a08100aea0e62817e21a54
pdrmenezes <pdrmenezes1@gmail.com>
2023-11-06 20:16:41 -0300

    redirect if not admin, authorize if they have a token

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,19 @@
 import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
-export default withAuth(function middleware(request) {
-  console.log(request.nextUrl.pathname);
-  console.log(request.nextauth.token.role);
-});
+export default withAuth(
+  function middleware(request) {
+    console.log(request.nextUrl.pathname);
+    console.log(request.nextauth.token.role);
+    if (request.nextUrl.pathname.startsWith("/create-user") && request.nextauth.token.role != "admin") {
+      return NextResponse.rewrite(new URL("/denied", request.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = { matcher: ["/create-user"] };
