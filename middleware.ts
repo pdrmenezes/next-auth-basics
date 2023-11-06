c51a4e6e043c58a158b9ab0322c9944921218741
pdrmenezes <pdrmenezes1@gmail.com>
2023-11-06 20:13:52 -0300

    creating custom middleware logic

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,10 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(function middleware(request) {
+  console.log(request.nextUrl.pathname);
+  console.log(request.nextauth.token.role);
+});
 
 export const config = { matcher: ["/create-user"] };
